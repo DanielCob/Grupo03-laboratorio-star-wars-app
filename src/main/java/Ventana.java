0072d15ce67950548341ac3376c053bf1698a13f
DanielCob <83792193+DanielCob@users.noreply.github.com>
2021-10-29 18:04:33 -0600

    Versión funcional

--- src/main/java/Ventana.java
+++ src/main/java/Ventana.java
@@ -1,6 +1,13 @@
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
 import javax.swing.*;
+import java.io.FileReader;
 
 public class Ventana {
+
+    public static Object[][] personajes_array = new Object[9][3];
 
     public static void main(String[] args)
     {
@@ -15,11 +22,34 @@ public class Ventana {
 
     public static JTable createTable()
     {
-        String[] columnNames = {"First Name", "Last Name"};
+        getJSON();
+        String[] columnNames = {"Nombre", "Estatura", "Año de nacimiento"};
         Object[][] data = {{"Kathy", "Smith"},{"John", "Doe"}};
-        JTable table = new JTable(data, columnNames);
+        JTable table = new JTable(personajes_array, columnNames);
         table.setFillsViewportHeight(true);
 
         return table;
+    }
+    public static void getJSON(){
+        JSONParser parser = new JSONParser();
+        try {
+            Object obj = parser.parse(new FileReader("src/main/java/starwars.json"));
+
+            JSONArray json = (JSONArray) obj;
+
+            for (int i = 0; i < json.size(); i++) {
+                String[] subarray = new String[3];
+                JSONObject object =(JSONObject) json.get(i);
+                String nombre = object.get("Nombre").toString();
+                String estatura = object.get("Estatura").toString();
+                String edad = object.get("Año de nacimiento").toString();
+                subarray[0] = nombre;
+                subarray[1] = estatura;
+                subarray[2] = edad;
+                personajes_array[i] = subarray;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
